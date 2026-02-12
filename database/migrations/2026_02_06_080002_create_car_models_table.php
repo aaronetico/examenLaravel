34759386b372924df4747516b95b3a44ada6d175
aaronetico <95538974+aaronetico@users.noreply.github.com>
2026-02-12 16:08:40 +0000

    añadir restricts para entrega

--- database/migrations/2026_02_06_080002_create_car_models_table.php
+++ database/migrations/2026_02_06_080002_create_car_models_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     {
         Schema::create('car_models', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->constrained()->onDelete('restrict');
             $table->string('name');
 
 
--- database/migrations/2026_02_06_084043_create_car_versions_table.php
+++ database/migrations/2026_02_06_084043_create_car_versions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     {
         Schema::create('car_versions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('car_year_id')->constrained('car_years')->onDelete('cascade');
+            $table->foreignId('car_year_id')->constrained('car_years')->onDelete('restrict');
             $table->string('name');
 
 
